73bc462d1c181b46713420de63cd79adce1ec2bd
Automationenthus <psdevaki@gmail.com>
2025-04-16 01:08:42 -0400

    MystoreProject

---
+++ src/main/java/com/mystore/base/BaseClass.java
@@ -0,0 +1,17 @@
+package com.mystore.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.ca/");
+		
+		
+	}
+}
